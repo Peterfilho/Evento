1ee93d680f1c80a93b83a94109b669cfd51f9fb9
Erika Leão <erika.lesantos@gmail.com>
2018-06-19 19:16:22 -0300

    patrocinador- master

--- resources/views/evento/index.blade.php
+++ resources/views/evento/index.blade.php
@@ -26,7 +26,26 @@
             <td>
                 <a class="btn btn-primary btn-crud " href="{{ route('events.show',$evento['id']) }}">Exibir</a>
                 <a class="btn btn-primary btn-crud " href="{{ route('events.edit',$evento['id']) }}">Editar</a>
-                <a class="btn btn-danger btn-crud " data-toggle="modal" data-target="#exampleModal" href="">Deletar</a>
+                <a class="btn btn-danger btn-crud " data-toggle="modal" data-target="#exampleModal{{{$evento['id']}}}" href="TESTE">Deletar</a>
+                <div class="modal fade" id="exampleModal{{{$evento['id']}}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente excluir o Evento {{$evento['name']}}?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">Esta ação não pode ser desfeita.</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" href="#" type="button" data-dismiss="modal">Cancel</button>
+                                {!! Form::open(array('route' => array('events.destroy', $evento['id']),  'method' => 'delete')) !!}
+                                <button type="submit" class="btn btn-danger">Deletar</button>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </td>
           </tr>
           @endforeach
@@ -34,24 +53,6 @@
     </div>
   </div>
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-          <div class="modal-content">
-              <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Deseja realmente excluir o Evento {{$evento['name']}}?</h5>
-                  <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">×</span>
-                  </button>
-              </div>
-              <div class="modal-body">Esta ação não pode ser desfeita.</div>
-              <div class="modal-footer">
-                  <button class="btn btn-secondary" href="#" type="button" data-dismiss="modal">Cancel</button>
-                  {!! Form::open(array('route' => array('events.destroy', $evento['id']),  'method' => 'delete')) !!}
-                  <button type="submit" class="btn btn-danger">Deletar</button>
-                  {!! Form::close() !!}
-              </div>
-          </div>
-      </div>
-  </div>
+
 
 @endsection
